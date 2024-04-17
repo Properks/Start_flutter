3850410b2608867e23c7bc11606d5b658e579bda
Jeongmo.Seo <wjdah1402@gmail.com>
2024-04-17 12:32:10 +0900

    :sparkles: feat: PageView 및 다크모드 구현 - PageView로 이미지 삽입하여 슬라이드 구현 - SystemChrome을 이용해 다크모드 적용

--- project/image_carousel/lib/screen/home_screen.dart
+++ project/image_carousel/lib/screen/home_screen.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class HomeScreen extends StatelessWidget {
   HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.dark);
     return Scaffold(
-      body: Text("Home Screen"),
+      body: PageView(
+        children: [1, 2, 3, 4, 5]
+        .map((number) =>
+            Image.asset(
+              "assets/img/image_$number.jpeg",
+              fit: BoxFit.fill,
+            ),
+        ).toList(),
+      ),
     );
   }
 }
