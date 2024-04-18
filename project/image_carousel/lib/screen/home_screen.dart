26d1f5b0a3e3fccba479f8d9901ec3c7abd82093
Jeongmo.Seo <wjdah1402@gmail.com>
2024-04-18 15:00:53 +0900

    :sparkles: feat: 자동으로 스와이프되는 기능 구현 - StatefulWidget으로 state 초기화 및 build 설정 - Timer, PageController로 몇 초 뒤 자동으로 넘어가도록 설정

--- project/image_carousel/lib/screen/home_screen.dart
+++ project/image_carousel/lib/screen/home_screen.dart
@@ -1,14 +1,50 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-class HomeScreen extends StatelessWidget {
+// Stateful widget
+class HomeScreen extends StatefulWidget {
   HomeScreen({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _HomeScreenState();
+  }
+}
+
+class _HomeScreenState extends State<HomeScreen>{
+  final PageController pageController = PageController();
+
+  @override
+  void initState() {
+    super.initState();
+
+    Timer.periodic( // duration이 끝나면 뒤의 함수를 실행
+      Duration(seconds: 3),
+        (timer) {
+          int? currentPage = pageController.page?.toInt();
+
+          if (currentPage == null) {return;} // null인 상황 제외
+
+          currentPage = (currentPage + 1) % 5;// 페이지 증가 후 마지막 페이지에서는 0으로 분기
+
+          pageController.animateToPage(
+              currentPage, // 이동할 위치
+              duration: const Duration(milliseconds: 500), // 이동에 걸리는 시간
+              curve: Curves.ease // 이동하는 방식
+          );
+          print("${currentPage + 1}번 째 페이지로 이동!");
+        }
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.dark);
     return Scaffold(
       body: PageView(
+        controller: pageController,
         children: [1, 2, 3, 4, 5]
         .map((number) =>
             Image.asset(
