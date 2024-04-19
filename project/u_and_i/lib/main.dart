6c4275a1b6da48a5b58132159b025ad3d25be3ac
Jeongmo.Seo <wjdah1402@gmail.com>
2024-04-19 12:53:43 +0900

    :sparkles: feat: u_and_i 프로젝트 초기화 - 프로젝트 초기 설정 - 사진, 폰트 추가

---
+++ project/u_and_i/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:u_and_i/screen/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
