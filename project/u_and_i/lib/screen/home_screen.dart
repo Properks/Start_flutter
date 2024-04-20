4b337d8391feba51327207c6558136e8e55b69ed
Jeongmo.Seo <wjdah1402@gmail.com>
2024-04-20 14:55:28 +0900

    :sparkles: feat: U and I 기본적인 페이지 구현 - 기본적인 버튼, 텍스트 및 사진 추가

--- project/u_and_i/lib/screen/home_screen.dart
+++ project/u_and_i/lib/screen/home_screen.dart
@@ -5,7 +5,62 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("Home Screen"),
+      backgroundColor: Colors.pink[100],
+      body: SafeArea(
+        top:true,
+        bottom: true,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              _DDay(),
+              _CoupleImage(),
+            ]
+          ),
+      ),
+    );
+  }
+}
+
+class _DDay extends StatelessWidget {
+  _DDay({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const SizedBox(height: 16),
+        const Text("U & i"),
+        const SizedBox(height: 16),
+        const Text("처음 만난 날"),
+        const Text("2022.12.31"),
+        const SizedBox(height: 16),
+        IconButton(
+          iconSize: 60,
+          onPressed: () {},
+          icon: const Icon(
+              Icons.favorite
+          ),
+        ),
+        const SizedBox(height: 16),
+        const Text("D + 365"),
+      ],
+    );
+  }
+}
+
+class _CoupleImage extends StatelessWidget {
+  _CoupleImage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Image.asset(
+        "assets/img/middle_image.png",
+
+        // 화면의 크기를 가지고 와서 높이에 2를 나눈 값
+        height: MediaQuery.of(context).size.height / 2,
+      ),
     );
   }
 }
