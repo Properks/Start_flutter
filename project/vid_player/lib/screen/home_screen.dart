e6b4e7bde60d0c12a14a88696d2aaa9f1315fbf3
Jeongmo.Seo <wjdah1402@gmail.com>
2024-05-01 20:03:08 +0900

    :sparkles: feat: home screen의 비디오 없는 화면 구현 - 함수 및 클래스로 home screen 구현 - home screen의 그라데이션 추가

--- project/vid_player/lib/screen/home_screen.dart
+++ project/vid_player/lib/screen/home_screen.dart
@@ -1,13 +1,101 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget{
 
   const HomeScreen({super.key});
 
   @override
+  State<StatefulWidget> createState() {
+    return _HomeScreenState();
+  }
+
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  XFile? video;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text("Home Screen")),
+      backgroundColor: Colors.black,
+      body: video != null ? renderVideo() : renderEmpty(),
+    );
+  }
+
+  Widget renderEmpty() { // video가 없을 때의 함수
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      decoration: getBoxDecoration(),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          _Logo(),
+          const SizedBox(height: 30),
+          _AppName()
+        ],
+      ),
+    );
+  }
+
+  BoxDecoration getBoxDecoration() {
+    return const BoxDecoration(
+      gradient: LinearGradient(
+        begin: Alignment.topCenter,
+        end: Alignment.bottomCenter,
+        colors: [
+          Color(0xFF2A3A7C),
+          Color(0xFF000118),
+        ],
+      ),
+    );
+  }
+
+  Widget renderVideo() { // video가 있을 때 함수
+    return Container();
+  }
+}
+
+class _Logo extends StatelessWidget{
+  const _Logo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset(
+      "asset/img/logo.png",
+    );
+  }
+}
+
+class _AppName extends StatelessWidget {
+  const _AppName({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    const textStyle = TextStyle(
+      color: Colors.white,
+      fontSize: 30,
+      fontWeight: FontWeight.w300,
+    );
+
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Text(
+          "VIDEO",
+          style: textStyle,
+        ),
+        Text(
+          "PLAYER",
+          style: textStyle.copyWith(fontWeight: FontWeight.w700),
+        )
+      ],
     );
   }
 }
