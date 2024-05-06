709d38283e0baeb6dc3e797f5a001d3cf9b61ae8
Jeongmo Seo <wjdah1402@gmail.com>
2024-05-06 14:44:50 +0900

    :sparkles: feat: 지도에 위치 설정 및 출석 체크 버튼 구현 - 위치 허가를 위한 info.plist 수정 - 지도에 Marker와 Circle을 표시 - 출석 체크 버튼 구현

--- project/chool_check/lib/screen/home_screen.dart
+++ project/chool_check/lib/screen/home_screen.dart
@@ -9,6 +9,22 @@ class HomeScreen extends StatelessWidget {
     126.921252,
   );
 
+  static const Marker marker = Marker(
+    markerId: MarkerId("Company"),
+    position: companyPosition,
+  );
+
+  static const double radiusOfCircle = 100;
+
+  static Circle circle = Circle(
+    circleId: const CircleId("choolCheckCircle"),
+    center: companyPosition,
+    radius: radiusOfCircle,
+    fillColor: Colors.cyan.withOpacity(0.5),
+    strokeColor: Colors.cyan,
+    strokeWidth: 1,
+  );
+
   const HomeScreen({super.key});
 
   @override
@@ -19,7 +35,7 @@ class HomeScreen extends StatelessWidget {
         future: checkPermission(),
         builder: (context, snapshot) {
           if (!snapshot.hasData && snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator(),);
+            return Center(child: CircularProgressIndicator(),); // 위치 정보가 없으면 progress 실행
           }
           if (snapshot.data == "위치 권한이 허가되었습니다.") {
             return Column(
@@ -31,6 +47,9 @@ class HomeScreen extends StatelessWidget {
                       target: companyPosition,
                       zoom: 16,
                     ),
+                    markers: {marker}, // 좌표에 마커 생성
+                    circles: {circle}, // 좌표 주위의 원 생성
+                    myLocationEnabled: true, // 내 위치 보여주기
                   ),
                 ),
                 Expanded(
@@ -44,7 +63,46 @@ class HomeScreen extends StatelessWidget {
                       ),
                       const SizedBox(height: 20),
                       ElevatedButton(
-                        onPressed: () {},
+                        onPressed: () async {
+                          final currentPosition = await Geolocator
+                              .getCurrentPosition();
+
+                          final distance = Geolocator.distanceBetween(
+                              currentPosition.latitude,
+                              currentPosition.longitude,
+                              companyPosition.latitude,
+                              companyPosition.longitude
+                          );
+
+                          bool canCheck = distance < radiusOfCircle;
+
+                          showDialog(
+                            context: context,
+                            builder: (_) {
+                              return AlertDialog(
+                                title: Text("출석 체크"),
+                                content: Text(
+                                  canCheck ? "출석 체크 하시겠습니까?" : "출석 체크를 할 수 없습니다."
+                                ),
+                                actions: [
+                                  TextButton(
+                                    onPressed: () {
+                                      Navigator.of(context).pop(false);
+                                    },
+                                    child: Text("취소"),
+                                  ),
+                                  if (canCheck)
+                                    TextButton(
+                                      onPressed: () {
+                                        Navigator.of(context).pop(true);
+                                      },
+                                      child: Text("출석 체크"),
+                                    ),
+                                ],
+                              );
+                            }
+                          );
+                        },
                         child: const Text("출석 체크"),
                       ),
                     ],
@@ -74,24 +132,24 @@ class HomeScreen extends StatelessWidget {
   }
 
   Future<String> checkPermission() async {
-    final isLocationEnabled = await Geolocator.isLocationServiceEnabled();
+    final isLocationEnabled = await Geolocator.isLocationServiceEnabled(); // 위치 서비스의 이용 가능 여부
 
     if (!isLocationEnabled) {
       return "위치 서비스를 활성화해주세요";
     }
 
-    LocationPermission permission = await Geolocator.checkPermission();
+    LocationPermission permission = await Geolocator.checkPermission(); // 허가가 되었는지 체크
 
-    if (permission == LocationPermission.denied) {
-      Geolocator.requestPermission();
+    if (permission == LocationPermission.deniedForever) {
+      return "앱의 위치 권한을 설정에서 허가해주세요";
+    }
+
+    if (permission != LocationPermission.whileInUse && permission != LocationPermission.always) {
+      permission = await Geolocator.requestPermission(); // 허가 요청
 
       if (permission == LocationPermission.denied) {
         return "위치 권한을 허가해주세요";
       }
-    }
-
-    if (permission == LocationPermission.deniedForever) {
-      return "앱의 위치 권한을 설정에서 허가해주세요";
     }
 
     return "위치 권한이 허가되었습니다.";
