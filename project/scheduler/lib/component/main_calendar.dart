a4cbebb96c9b310f5983e325d5772b445776c1a0
Jeongmo Seo <wjdah1402@gmail.com>
2024-05-14 15:37:19 +0900

    :sparkles: feat: 일정 관리 캘린더 생성 및 날짜 선택 구현 - 캘린더 생성 및 스타일 변경 - 날짜 선택 기능 구현

---
+++ project/scheduler/lib/component/main_calendar.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:scheduler/const/colors.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+class MainCalendar extends StatelessWidget {
+  final OnDaySelected onDaySelected;
+  final DateTime selectedDate;
+
+  const MainCalendar({
+    required this.onDaySelected,
+    required this.selectedDate,
+    super.key
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TableCalendar(
+
+      onDaySelected: onDaySelected, // 날짜가 선택되었을 때 실행할 함수
+      selectedDayPredicate: (date) => // boolean으로 반환해 true면 선택된 날짜, false면 선택되지 않은 날짜로 선정
+        date.year == selectedDate.year &&
+        date.month == selectedDate.month &&
+        date.day == selectedDate.day,
+      focusedDay: DateTime.now(),
+      firstDay: DateTime(1000, 1, 1),
+      lastDay: DateTime(3000, 1, 1),
+      headerStyle: const HeaderStyle(
+        titleCentered: true, // 제목 중간
+        formatButtonVisible: false, // 달력 크기 옵션 삭제
+        titleTextStyle: TextStyle(
+          fontWeight: FontWeight.w700,
+          fontSize: 16
+        ),
+      ),
+      calendarStyle: CalendarStyle(
+        isTodayHighlighted: false,
+        defaultDecoration: BoxDecoration(
+          color: LIGHT_GREY_COLOR,
+          borderRadius: BorderRadius.circular(6),
+        ),
+        weekendDecoration: BoxDecoration(
+            color: LIGHT_GREY_COLOR,
+            borderRadius: BorderRadius.circular(6),
+        ),
+        selectedDecoration: BoxDecoration(
+          color: LIGHT_GREY_COLOR,
+          borderRadius: BorderRadius.circular(6),
+          border: Border.all(
+            color: PRIMARY_COLOR,
+          ),
+        ),
+        defaultTextStyle: TextStyle(
+          fontWeight: FontWeight.w600,
+          color: DARK_GREY_COLOR,
+        ),
+        weekendTextStyle: TextStyle(
+          fontWeight: FontWeight.w600,
+          color: DARK_GREY_COLOR,
+        ),
+        selectedTextStyle: const TextStyle(
+          fontWeight: FontWeight.w600,
+          color: PRIMARY_COLOR,
+        ),
+      ),
+    );
+  }
+}
--- project/scheduler/lib/screen/home_screen.dart
+++ project/scheduler/lib/screen/home_screen.dart
@@ -1,13 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:scheduler/component/main_calendar.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget{
 
   const HomeScreen({super.key});
 
   @override
+  State<StatefulWidget> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  DateTime selectedDate = DateTime.utc(
+    DateTime.now().year,
+    DateTime.now().month,
+    DateTime.now().day,
+  );
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text("Home Screen"),),
+      body: SafeArea(
+        child: Column(
+          children: [
+            MainCalendar(
+              selectedDate: selectedDate,
+              onDaySelected: onDaySelected,
+            ),
+          ],
+        ),
+      ),
     );
+  }
+
+  void onDaySelected(DateTime selectedDate, DateTime focusedDate) {
+    setState(() {
+      this.selectedDate = selectedDate;
+      print("Selectd: " + selectedDate.toString() + ", Focus: " + focusedDate.toString());
+      // 아직은 두개의 값이 같다.
+    });
   }
 }
