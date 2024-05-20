06722dfd2b91c162ffe11919677d109df02ea4c5
Jeongmo Seo <wjdah1402@gmail.com>
2024-05-20 13:10:01 +0900

    :sparkles: feat: 일정 생성 UI 생성 - 시간, 내용 입력 UI 생성

--- project/scheduler/lib/component/custom_text_field.dart
+++ project/scheduler/lib/component/custom_text_field.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:scheduler/const/colors.dart';
 
 class CustomTextField extends StatelessWidget {
 
   final String label;
+  final bool isTime;
 
   const CustomTextField({
     required this.label,
+    required this.isTime,
     super.key
   });
 
@@ -22,7 +25,24 @@ class CustomTextField extends StatelessWidget {
             fontWeight: FontWeight.w600,
           ),
         ),
-        TextFormField(),
+        Expanded(
+          flex: isTime ? 0 : 1,
+          child: TextFormField(
+            cursorColor: Colors.grey,
+            maxLines: isTime ? 1 : null, // null이면 제한 없음
+            expands: !isTime, // 시간이면 부모 공간 만큼 차지, 아니면 최소 공간
+            keyboardType: isTime ? TextInputType.number : TextInputType.multiline, // 시간이면 숫자 아니면 키보드
+            inputFormatters: isTime ? [ // 시간인 경우 숫자로 제한
+              FilteringTextInputFormatter.digitsOnly
+            ] : [],
+            decoration: InputDecoration(
+              border: InputBorder.none, // 테두리 없음
+              filled: true, // 배경색 on
+              fillColor: Colors.grey[300], // 배경색
+              suffixText: isTime ? "시" : null, // 시간이면 뒤에 시를 붙이기
+            ),
+          ),
+        ),
       ],
     );
   }
--- project/scheduler/lib/component/schedule_bottom_sheet.dart
+++ project/scheduler/lib/component/schedule_bottom_sheet.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:scheduler/component/custom_text_field.dart';
+import 'package:scheduler/const/colors.dart';
 
 class ScheduleBottomSheet extends StatefulWidget {
   const ScheduleBottomSheet({
@@ -18,8 +19,51 @@ class _ScheduleBottomSheetState extends State<ScheduleBottomSheet> {
       child: Container(
         height: MediaQuery.of(context).size.height / 2,
         color: Colors.white,
-        child: CustomTextField(label: "시작 시간"),
+        child: Padding(
+          padding: const EdgeInsets.only(left: 8, right: 8, top: 8),
+          child: Column(
+            children: [
+              const Row(
+                children: [
+                  Expanded(
+                    child: CustomTextField(label: "시작 시간", isTime: true,)
+                  ),
+                  SizedBox(width: 16,),
+                  Expanded(
+                      child: CustomTextField(label: "종료 시간", isTime: true,)
+                  ),
+                ],
+              ),
+              const Expanded(
+                child: CustomTextField(
+                  label: "내용",
+                  isTime: false,
+                ),
+              ),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: onSavedPressed,
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: PRIMARY_COLOR,
+                  ),
+                  child: const Text(
+                    "저장",
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
       )
     );
   }
+
+  void onSavedPressed() {
+
+  }
+
 }
