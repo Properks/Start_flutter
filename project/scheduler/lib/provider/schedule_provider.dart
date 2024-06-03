f961b8d54e67446eed9bb6035a98f3f01d9771a1
Jeongmo Seo <wjdah1402@gmail.com>
2024-06-03 12:44:06 +0900

    :sparkles: feat: 스케줄 생성, 삭제 provider 구현 - 스케줄 생성, 삭제 provider 구현 및 캐시 반영

--- project/scheduler/lib/provider/schedule_provider.dart
+++ project/scheduler/lib/provider/schedule_provider.dart
@@ -27,7 +27,45 @@ class ScheduleProvider extends ChangeNotifier {
   }) async {
     final resp = await repository.getSchedules(date: date);
     cache.update(date, (value) => resp, ifAbsent: () => resp);// 존재하면 value를 resp로 없으면 resp 추가
+    notifyListeners(); // 현재 listen하고 있는 위젯들을 다시 build
+  }
+
+  void createSchedule({
+    required ScheduleModel model
+}) async {
+    final targetDate = model.date;
+    final resp = await repository.createSchedule(model: model);
+    // resp은 저장된 스케쥴의 Id
+
+    cache.update(targetDate, (value) => [ // targetDate에 해당하는 원래 일정 다 가져오기
+      ...value, // 원래 일정 뒤에 추가
+      model.copyWith(id: resp), // 스케쥴 내용에 id만 추가해서 삽입
+      ]..sort(
+        (a, b) => a.startTime.compareTo(b.startTime)
+      ), ifAbsent: () => [model.copyWith(id: resp)] // 해당 날짜의 일정이 없는 경우
+    );
+
     notifyListeners();
   }
 
+  void deleteSchedule({
+    required DateTime date,
+    required String id
+}) async {
+    final resp = repository.deleteSchedule(id: id);
+
+    cache.update(date, (value) =>
+      value.where((e) => e.id != id).toList(),
+      ifAbsent: () => []
+    );
+
+    notifyListeners();
+  }
+
+  void changeSchedule({
+    required DateTime date,
+}) {
+    selectedTime = date;
+    notifyListeners();
+  }
 }
